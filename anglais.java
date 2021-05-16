de260d0f3207bdf349eae7f531cab1630e0a16ce
kingsteph23 <75145009+kingsteph23@users.noreply.github.com>
2021-05-15 22:05:43 -0400

    Main compleletion of survey/quiz.
    Prettyyyy much done with main survey/quiz questions.
    Just need to add tips for users depending on score. Which include following:
    13-18: Low category
    19-22:Middle/Average category
    23-28:High category
    Depending on users choices,  tips will be made and  show results(addiction habbit,  or less of a adfiction habit)
    
    Finished: 
    All ten questions calculations of users score
    Add switch statements
    Calculation of users score out of 28. *28 is highest score you can get

--- anglais.java
+++ anglais.java
@@ -6,6 +6,7 @@ public class anglais {
 
     public static void main(String[] args) {
         // INTRO
+
         System.out.println("""
                 ________________________________________
                 |                                       |\s
@@ -16,6 +17,14 @@ public class anglais {
 
         System.out.println();
         Scanner scan = new Scanner(System.in);
+        System.out.println("""
+                Note:When responding to a question, write everything EXACTLY AS YOU SEE IT
+                Also, you have the option to choose between the letter that is in the ()perenteces, or the full response
+                For example: (a)john doe is a goat.
+                 You can choose either(a) or john doe is a goat
+                but you still must write them the exact way you see them^  \s""");
+
+        System.out.println();
         System.out.println("Hello there, what is your name:");
         String name = scan.nextLine();
         System.out.println("Oh hey," + name + "!");
@@ -63,8 +72,11 @@ public class anglais {
         }
 
         int score = 0;
+
+
         //Question 1
-        System.out.println("Soo," + name + ",how long have you been using social media" +
+        System.out.println("Question #1:" +
+                "\sSoo," + name + ",how long have you been using social media" +
                 "\sNote:This is in general, no just a specific social network" +
                 "\s(1)1-2 years" +
                 "\s(2)3-6 years" +
@@ -88,7 +100,8 @@ public class anglais {
         // Question #2
 
         System.out.println();
-        System.out.println("Onto the next question," + name + "" +
+        System.out.println("Question #2:" +
+                "\nOnto the next question," + name + "" +
                 "\nWhen it comes your usage on these social networks(in general)" +
                 "\nHow long do you normally spend on social media(Be honest)" +
                 "\nLess than a hour" +
@@ -98,15 +111,14 @@ public class anglais {
         String question2 = scan.nextLine();
         if ("Less than a hour".equals(question2)) {
             score += 1;
-            score += 1;
+        } else {
+            if ("1-2 hours".equals(question2)) {
+                score += 2;
 
-        }else if ("1-2 hours".equals(question2)) {
-            score += 2;
-            score += 2;
 
-        }else if("3+ hrs".equals(question2)){
-            score +=3;
-            score += 3;
+            } else if ("3+ hrs".equals(question2)) {
+                score += 3;
+            }
         }
 
         //Question 3
@@ -132,15 +144,15 @@ public class anglais {
                 if("a".equals(mornin) || "Brush my teeth".equals(mornin)){
                     System.out.println("Get them teeth cleaned g \uD83D\uDE24\uD83D\uDE24 \uD83D\uDCAA\uD83C\uDFFE");
                     score += 1;
-                    score += 1;
+
 
             }else if("b".equals(mornin) ||  "Hop on the gram/snap and check to see whats going on in the world".equals(mornin)){
                     score += 3;
-                    score += 3;
+
                 }else if("morning routine".equals(mornin) || "other".equals(mornin)){
                     System.out.println("Understandable");
                     score += 2;
-                    score += 2;
+
             }
                     System.out.println();
 
@@ -167,25 +179,25 @@ public class anglais {
 
         String reaction = scan.nextLine();
 
-            if("a".equals(reaction) || "Check out the notification and end up being distracted from my work".equals(reaction)){
+        switch (reaction) {
+            case "a", "Check out the notification and end up being distracted from my work" -> {
                 System.out.println("Hey bro, put the phone down\uD83D\uDE11.And go back to work");
                 score += 3;
-                score += 3;
-
-            }else if("b".equals(reaction) || "Probably notice it, but still focus on my work. I'll check it after".equals(reaction)){
+            }
+            case "b", "Probably notice it, but still focus on my work. I'll check it after" -> {
                 System.out.println();
                 score += 2;
-                score += 2;
-            }else if("c".equals(reaction) || "What notification".equals(reaction)){
+            }
+            case "c", "What notification" -> {
                 System.out.println("Here's a \uD83C\uDF6A\uD83E\uDD5B");
                 score += 1;
-               score += 1;
             }
+        }
                 //question 5
                     System.out.println();
 
         System.out.println("""
-                Question #5
+                Question #5:
                 Lets say, you made a post or a tweet
                 but  you didn't get the # of shares,tweets,likes you were expecting
                 What would you do in this situation?""");
@@ -199,10 +211,10 @@ public class anglais {
 
         if("a".equals(reality) || "Still keep the post up".equals(reality)){
             score += 1;
-            score += 1;
+
         }else{
             score += 3;
-            score += 3;
+
         }
 
         // question 6
@@ -218,33 +230,126 @@ public class anglais {
         System.out.println();
 
         String allnighter = scan.nextLine();
-        if("a".equals(allnighter) ||"yes".equals(allnighter)){
-            System.out.println("BOI PUT THE PHONE DOWN, AND  GO TO SLEEP");
-            score += 3;
-            score += 3;
-
-        }else{
-            System.out.println("THAT'S GOOD YOU GETTING REST, I RESPECT DAT");
-            score += 1;
-            score += 1;
+        switch (allnighter) {
+            case "a", "yes" -> {
+                System.out.println("BOI PUT THE PHONE DOWN, AND  GO TO SLEEP");
+                score += 3;
+            }
+            default -> {
+                System.out.println("THAT'S GOOD YOU GETTING REST, I RESPECT DAT");
+                score += 1;
+            }
         }
             // question 7
                 System.out.println();
 
         System.out.println("""
-                #Question 7:If you were given a million bucks if you were able to leave all optional social medias/technolgies
+                #Question 7:
+                If you were given a million bucks if you were able to leave all optional social medias/technolgies
                 for 30 days, how do you think you will be/do. Be honest*
                 *NOTE:OPTIONAL TECHNOLGIES/SOCIAL MEDIA are apps/websites/services that aren't neccesary for work
                 Or a very important role in daily routine.  Optional technolgies consist of things we may use for entertainment \s""");
 
         System.out.println();
 
-        System.out.println("(a)I wouldn't really mind being away from them for a while. Free money" +
-                "(b)What about my likes,post and friends? i need to keep up with them" +
-                "(c)");
+        System.out.println("\s(a)I wouldn't really mind being away from them for a while. Free money" +
+                "\s(b)What about my likes,post and friends? i need to keep up with them" +"" +
+                "\n:");
 
         String optional = scan.nextLine();
+                if("a".equals(optional) || "I wouldn't really mind being away from them for a while. Free money".equals(optional)){
+                        System.out.println("Damn right easy money\uD83D\uDCB4 \uD83D\uDCB5\uD83D\uDCB4\uD83D\uDCB7\uD83D\uDCB6");
+                        score += 1;
 
+        }else{
+                    System.out.println("But what about the easy money \uD83E\uDDD0,\uD83D\uDCB4 \uD83D\uDCB5\uD83D\uDCB4\uD83D\uDCB7\uD83D\uDCB6");
+                    score += 3;
+
+                }
+                    System.out.println();
+                    // question 8
+        System.out.println("""
+                Question #8:
+                If on these social media platforms, do you compare yourself to others
+                Can consist of comparing yourself to the following: # of likes, what a friend is posting(party, vaction in Aruba)
+                # of followers between you and a friend, or trying to copy a very popular influncer.\s""");
+
+        System.out.println();
+        System.out.println("\s(a)I do compare myself on these platforms" +
+                "\s(b)I don't compare myself on these platforms" +
+                "\s:");
+
+        String comparison = scan.nextLine();
+
+        switch (comparison) {
+            case "a", "I do compare myself on these platforms" -> score += 1;
+            default -> score += 3;
+        }
+        // question #9
+        System.out.println("""
+                Question #9:
+                When scrolling through these social media sites/apps,
+                And lets say you're on Instagram, and you see one of your friends and you see them
+                at a party, and they're getting turn up, and you see a bunch of other people you might know/see,
+                and because of this, you start experiencing FOMO(Fear of missing out), and start feeling upset and angry
+                since you couldn't attends this event.
+                If in a similar situation like this, what would you do?
+                (a)don't even bother with the post and keep scrolling
+                (B)if i start experiencing emotions, i'll get off the site/app immediately and do something more productive
+                (c)keep looking at the post or similar post, and allow myself to experience  FOMO"
+                :""");
+
+        String FOMO = scan.nextLine();
+
+        if ("a".equals(FOMO) || "don't even bother with the post and keep scrolling".equals(FOMO)) {
+            score += 1;
+
+
+
+        } else if ("B".equals(FOMO) || "if i start experiencing emotions, i'll get off the site/app immediately and do something more productive ".equals(FOMO)) {
+            score += 2;
+
+
+        } else if ("c".equals(FOMO) || "keep looking at the post or similar post, and allow myself to experience  FOMO".equals(FOMO)) {
+            score += 3;
+
+
+            System.out.println();
+        }
+
+        // QUESTION #10 FINALE
+        System.out.println("""
+                Question #10:
+                Are you ready
+                yes
+                no
+                :""");
+
+        String finalconfirmation = scan.nextLine();
+        if("yes".equals(finalconfirmation) ||"no".equals(finalconfirmation)){
+            System.out.println("I gave the chance earlier to not take the quiz/survey,"+name+"......\uD83D\uDE11\uD83D\uDE11" +
+                    "\nBut NOPE! You decided to take my survey/quiz." +
+                    "\nSo guess what" +
+                    "\nWe gonna finish it\uD83D\uDE08\uD83D\uDE08");
+        }
+            System.out.println("Final question....." +
+                    "\sDoes the color red trigger/capture your attention(notification icons)?\uD83E\uDDD0" +
+                    "\s(a)It captures me" +
+                    "\s(b)i might notice it, but i wont be triggered" +
+                    "\s(c)Nope it doesn't capture/trigger my attention ");
+        System.out.println();
+        String triggercolor = scan.nextLine();
+
+        switch (triggercolor) {
+            case "a", "It captures me" -> score += 3;
+            case "b", "i might notice it, but i wont be triggered" -> score += 2;
+            case "c", "Nope it doesn't capture/trigger my attention" -> score += 1;
+
+        }
+
+        int fin = score;
+        System.out.println("Final score:"+ (fin) +"/28");
 
     }
 }
+
